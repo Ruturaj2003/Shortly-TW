7c3c7ceaa4e42b4c1ff106e04c3f4fccc70bdaa0
Ruturaj <ruturajhb2003@gmail.com>
2023-06-13 21:11:12 +0530

    Add Mobile and Hamburger Menu

--- js/script.js
+++ js/script.js
@@ -1,39 +1,46 @@
-const input = document.getElementById('input');
-const linkForm = document.getElementById('link-form');
-const errMsg = document.getElementById('err-msg');
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
 
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
 
+btn.addEventListener('click', navToggle)
+linkForm.addEventListener('submit', formSubmit)
 
-const dota = document.getElementsByClassName('dota');
-
-linkForm.addEventListener('submit', formSubmit);
-
-
-function formSubmit(event) {
-    event.preventDefault();
-
-
-    if (input.value === "") {
-        errMsg.innerHTML = "Please Enter Something";
-        input.classList.add('border-red');
-    }
-
-    else if (!(isUrlValid(input.value))) {
-        errMsg.innerHTML = "Please enter Valid URL";
-        input.classList.add('border-red');
-    }
-    else {
-        errMsg.innerHTML = "";
-        input.classList.remove('border-red');
-        setTimeout(function () { alert("Success"); }, 1000);
-
-    }
+// Toggle Mobile Menu
+function navToggle() {
+    btn.classList.toggle('open')
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
 }
 
-function isUrlValid(userInput) {
-    var res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    if (res == null)
-        return false;
-    else
-        return true;
+// Validate a URL
+function validURL(str) {
+    var pattern = new RegExp(
+        '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+        'i'
+    )
+    return !!pattern.test(str)
+}
+
+function formSubmit(e) {
+    e.preventDefault()
+
+    if (input.value === '') {
+        errMsg.innerHTML = 'Please enter something'
+        input.classList.add('border-red')
+    } else if (!validURL(input.value)) {
+        errMsg.innerHTML = 'Please enter a valid URL'
+        input.classList.add('border-red')
+    } else {
+        errMsg.innerHTML = ''
+        input.classList.remove('border-red')
+        alert('Success')
+    }
 }
